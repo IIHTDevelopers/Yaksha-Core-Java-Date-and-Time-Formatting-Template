9842b3381ad22ca7d382a0ecea8f95c1cc5fcfe1
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-01-07 14:55:38 +0530

    Update DateTimeFormattingAndParsingAssignment.java

--- src/main/java/com/yaksha/assignment/DateTimeFormattingAndParsingAssignment.java
+++ src/main/java/com/yaksha/assignment/DateTimeFormattingAndParsingAssignment.java
@@ -9,21 +9,6 @@ public class DateTimeFormattingAndParsingAssignment {
 
 	public static void main(String[] args) {
 
-		// Task 1: Date Formatting
-		LocalDateTime now = LocalDateTime.now();
-		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-		System.out.println("Formatted Date and Time: " + now.format(dateFormatter));
-
-		// Task 2: Parse Date Strings
-		String dateStr = "2025-01-02 12:30 PM";
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm a");
-		LocalDateTime dateTime = LocalDateTime.parse(dateStr, formatter);
-		System.out.println("Parsed Date and Time: " + dateTime);
-
-		// Task 3: Time Zone Formatting
-		ZonedDateTime nowInTokyo = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
-		DateTimeFormatter timeZoneFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss Z");
-		String formattedDateInTokyo = nowInTokyo.format(timeZoneFormatter);
-		System.out.println("Formatted Date and Time in Tokyo: " + formattedDateInTokyo);
+		// Write your logic here
 	}
 }
